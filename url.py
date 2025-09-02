3550e1a07b0c076a3ca139bba7cc9295e228f2b4
Abhinandan Mehandiratta <39618909+Enigmaderockz@users.noreply.github.com>
2025-09-02 17:50:44 +0530

    Create url.py

---
+++ url.py
@@ -0,0 +1,42 @@
+# To run this code you need to install the following dependencies:
+# pip install google-genai
+
+import base64
+import os
+from google import genai
+from google.genai import types
+
+
+def generate():
+    client = genai.Client(
+        api_key=os.environ.get("GEMINI_API_KEY"),
+    )
+
+    model = "gemini-2.5-flash"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text="""What are the top 3 recent announcements from the Gemini API? For each of these changes, explain why it is important and suggest a use case for each of the changes. Here is the URL for your research: https://ai.google.dev/gemini-api/docs/changelog"""),
+            ],
+        ),
+    ]
+    tools = [
+        types.Tool(url_context=types.UrlContext()),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        thinking_config = types.ThinkingConfig(
+            thinking_budget=-1,
+        ),
+        tools=tools,
+    )
+
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        print(chunk.text, end="")
+
+if __name__ == "__main__":
+    generate()
